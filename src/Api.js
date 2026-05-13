5e93b24246fcde6e2c622a4fbbee867f0092bf8f
Manisha_athili <manishaathili1126@gmail.com>
2026-05-13 14:25:19 +0530

    added api

--- src/Api.js
+++ src/Api.js
@@ -1,39 +1,40 @@
 
 
 export const NewsArticles = async (title, setArticles, setSelected, setGenerated) => {
-   try {
-    const url = `https://ai-news-studio-server.onrender.com/api/news?topic=${encodeURIComponent(title)}` || `http://localhost:4000/news/?title=${encodeURIComponent(title)}`;
-    const res = await fetch(url);
+    try {
+        const baseUrl = import.meta.env.VITE_API_URL || "http://localhost:4000";
+        const url = `${baseUrl}/api/news?topic=${encodeURIComponent(title)}`;
 
-    if(!res.ok) throw new Error("Failed to fetch the articles");
+        const res = await fetch(url);
+        if (!res.ok) throw new Error("Failed to fetch the articles");
 
-    const data = await res.json();
-    setArticles(data.slice(0,5));
-    setSelected([]);
-    setGenerated(null) ;
-    
-   } catch (error) {
-    console.log(error,"failed to fetch articles");
-    alert(error,"failed to fetch articles")
-   }
-    
+        const data = await res.json();
+        setArticles(data.slice(0, 5));
+        setSelected([]);
+        setGenerated(null);
+
+    } catch (error) {
+        console.log(error, "failed to fetch articles");
+        alert(error, "failed to fetch articles")
+    }
+
 }
 
-export const generateArticles = async(selectedArticles,setGenerated)=>{
-   
+export const generateArticles = async (selectedArticles, setGenerated) => {
+
     try {
-        const url = `https://ai-news-studio-server.onrender.com/api/ai/`
-        const res = await fetch(url,
+        const baseUrl = import.meta.env.VITE_API_URL || "http://localhost:4000";
+        const res = await fetch(`${baseUrl}/api/ai/`,
             {
-                method : "POSt",
-                headers : {"content-Type": "application/json"},
-                body : JSON.stringify({ selectedArticles, title })
+                method: "POST",
+                headers: { "content-Type": "application/json" },
+                body: JSON.stringify({ selectedArticles, title })
             }
         );
 
-        if(!res.ok) throw new Error("Failed to Generate AI Articles");
-        
-        const result = JSON.parse(res);
+        if (!res.ok) throw new Error("Failed to Generate AI Articles");
+
+        const result = await res.json();
         setGenerated(result);
     } catch (error) {
         console.error("Generate error:", error);
